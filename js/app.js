714a62e09732db8a2f4781cfc826414d7d72c63c
Viet Hung <viethung01234@gmail.com>
2023-11-24 19:54:28 +0700

    ADD signup submit handler

--- js/app.js
+++ js/app.js
@@ -5,101 +5,135 @@
    File Description: Main js file
 */
 
-
-
 //  Window scroll sticky class add
+
 function windowScroll() {
-    const navbar = document.getElementById("navbar");
-    if (
-        document.body.scrollTop >= 50 ||
-        document.documentElement.scrollTop >= 50
-    ) {
-        navbar.classList.add("nav-sticky");
-    } else {
-        navbar.classList.remove("nav-sticky");
-    }
+  const navbar = document.getElementById("navbar");
+  if (
+    document.body.scrollTop >= 50 ||
+    document.documentElement.scrollTop >= 50
+  ) {
+    navbar.classList.add("nav-sticky");
+  } else {
+    navbar.classList.remove("nav-sticky");
+  }
 }
 
-window.addEventListener('scroll', (ev) => {
-    ev.preventDefault();
-    windowScroll();
-})
-
-
-// Smooth scroll 
-var scroll = new SmoothScroll('#navbar-navlist a', {
-    speed: 500
+window.addEventListener("scroll", (ev) => {
+  ev.preventDefault();
+  windowScroll();
 });
 
+// Smooth scroll
+var scroll = new SmoothScroll("#navbar-navlist a", {
+  speed: 500,
+});
 
 // Contact Form
 function validateForm() {
-    var name = document.forms["myForm"]["name"].value;
-    var email = document.forms["myForm"]["email"].value;
-    var subject = document.forms["myForm"]["subject"].value;
-    var comments = document.forms["myForm"]["comments"].value;
-    document.getElementById("error-msg").style.opacity = 0;
-    document.getElementById('error-msg').innerHTML = "";
-    if (name == "" || name == null) {
-        document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Name*</div>";
-        fadeIn();
-        return false;
-    }
-    if (email == "" || email == null) {
-        document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Email*</div>";
-        fadeIn();
-        return false;
-    }
-    if (subject == "" || subject == null) {
-        document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Subject*</div>";
-        fadeIn();
-        return false;
-    }
-    if (comments == "" || comments == null) {
-        document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Comments*</div>";
-        fadeIn();
-        return false;
-    }
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("simple-msg").innerHTML = this.responseText;
-            document.forms["myForm"]["name"].value = "";
-            document.forms["myForm"]["email"].value = "";
-            document.forms["myForm"]["subject"].value = "";
-            document.forms["myForm"]["comments"].value = "";
-        }
-    };
-    xhttp.open("POST", "php/contact.php", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("name=" + name + "&email=" + email + "&subject=" + subject + "&comments=" + comments);
+  var name = document.forms["myForm"]["name"].value;
+  var email = document.forms["myForm"]["email"].value;
+  var subject = document.forms["myForm"]["subject"].value;
+  var comments = document.forms["myForm"]["comments"].value;
+  document.getElementById("error-msg").style.opacity = 0;
+  document.getElementById("error-msg").innerHTML = "";
+  if (name == "" || name == null) {
+    document.getElementById("error-msg").innerHTML =
+      "<div class='alert alert-warning error_message'>*Please enter a Name*</div>";
+    fadeIn();
     return false;
+  }
+  if (email == "" || email == null) {
+    document.getElementById("error-msg").innerHTML =
+      "<div class='alert alert-warning error_message'>*Please enter a Email*</div>";
+    fadeIn();
+    return false;
+  }
+  if (subject == "" || subject == null) {
+    document.getElementById("error-msg").innerHTML =
+      "<div class='alert alert-warning error_message'>*Please enter a Subject*</div>";
+    fadeIn();
+    return false;
+  }
+  if (comments == "" || comments == null) {
+    document.getElementById("error-msg").innerHTML =
+      "<div class='alert alert-warning error_message'>*Please enter a Comments*</div>";
+    fadeIn();
+    return false;
+  }
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("simple-msg").innerHTML = this.responseText;
+      document.forms["myForm"]["name"].value = "";
+      document.forms["myForm"]["email"].value = "";
+      document.forms["myForm"]["subject"].value = "";
+      document.forms["myForm"]["comments"].value = "";
+    }
+  };
+  xhttp.open("POST", "php/contact.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(
+    "name=" +
+      name +
+      "&email=" +
+      email +
+      "&subject=" +
+      subject +
+      "&comments=" +
+      comments
+  );
+  return false;
 }
 
 function fadeIn() {
-    var fade = document.getElementById("error-msg");
-    var opacity = 0;
-    var intervalID = setInterval(function() {
-        if (opacity < 1) {
-            opacity = opacity + 0.5
-            fade.style.opacity = opacity;
-        } else {
-            clearInterval(intervalID);
-        }
-    }, 200);
+  var fade = document.getElementById("error-msg");
+  var opacity = 0;
+  var intervalID = setInterval(function () {
+    if (opacity < 1) {
+      opacity = opacity + 0.5;
+      fade.style.opacity = opacity;
+    } else {
+      clearInterval(intervalID);
+    }
+  }, 200);
 }
 
 // feather icon
 
 feather.replace();
 
-
 // Preloader
 
 window.onload = function loader() {
-    setTimeout(() => {
-        document.getElementById('preloader').style.visibility = 'hidden';
-        document.getElementById('preloader').style.opacity = '0';
-    }, 350);
-}
+  setTimeout(() => {
+    document.getElementById("preloader").style.visibility = "hidden";
+    document.getElementById("preloader").style.opacity = "0";
+  }, 350);
+};
+
+//  CallAPI
+const endpoint = "http://localhost:5000";
+const data = {
+  username: "Hung",
+  apikey: "USMs0fsbJIRuoJtG7nWmotlxPY8ia3tm",
+  password: "51856jafg125",
+};
+
+const handleSignUp = async () => {
+  try {
+    await axios.post(endpoint + "/sign-up", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    alert("Thanh cong ");
+  } catch (error) {
+    console.log(error);
+    alert("Có lỗi API");
+  }
+};
+const signUpButton = document.getElementById("signup-submit-btn");
+signUpButton.addEventListener("click", handleSignUp);
--- js/server.js
+++ js/server.js
@@ -3,15 +3,17 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const axios = require("axios");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 5000;
 
 // MongoDB connection setup (replace 'your_mongodb_uri' and 'yourdbname')
 mongoose.connect(
-    "mongodb+srv://slowey:tlvptlvp@projectx.3vv2dfv.mongodb.net/ProjectX", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
+  "mongodb+srv://slowey:tlvptlvp@projectx.3vv2dfv.mongodb.net/ProjectX",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
 // Create a schema and model for the Users table
@@ -23,26 +25,26 @@ mongoose.connect(
  * @property {string} password - The password of the user.
  */
 const UserSchema = new mongoose.Schema({
-    username: String,
-    apikey: String,
-    password: String,
+  username: String,
+  apikey: String,
+  password: String,
 });
 
-UserSchema.pre("save", async function(next) {
-    // Only run this function if password was actually modified
-    if (!this.isModified("password")) return next();
+UserSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
 
-    // Hash the password with cost of 12
-    this.password = await bcrypt.hash(this.password, 12);
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
 
-    next();
+  next();
 });
 
-UserSchema.methods.correctPassword = async function(
-    candidatePassword,
-    userPassword
+UserSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
 ) {
-    return await bcrypt.compare(candidatePassword, userPassword);
+  return await bcrypt.compare(candidatePassword, userPassword);
 };
 
 const User = mongoose.model("Users", UserSchema);
@@ -50,60 +52,66 @@ const User = mongoose.model("Users", UserSchema);
 // Middleware setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 // Serve static files (CSS, JS, etc.)
 app.use(express.static("public"));
 
 // Define a route for handling the registration form submission
-app.post("/sign-up", async(req, res) => {
-    try {
-        const { username, apikey, password } = req.body;
+app.post("/sign-up", async (req, res) => {
+  try {
+    const { username, apikey, password } = req.body;
 
-        // Check for missing fields
-        if (!username || !apikey || !password) {
-            return res.status(400).send("All fields are required");
-        }
-
-        // Check if user already exists
-        const existingUser = await User.findOne({ username });
-        if (existingUser) {
-            return res.status(409).send("User already exists");
-        }
-
-        // Validate API key
-        if (!(await isValidApiKey(apikey))) {
-            return res.status(401).send("Invalid API key");
-        }
-
-        // Create a new user document
-        const newUser = new User({ username, apikey, password });
-
-        // Save the user to the database
-        await newUser.save();
-
-        res.send("Registration successful");
-    } catch (error) {
-        console.error("Registration error:", error);
-        res.status(500).send("Registration failed");
+    // Check for missing fields
+    if (!username || !apikey || !password) {
+      return res.status(400).send("All fields are required");
     }
-});
 
+    // Check if user already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(409).send("User already exists");
+    }
+
+    // Validate API key
+    if (!(await isValidApiKey(apikey))) {
+      return res.status(401).send("Invalid API key");
+    }
+
+    // Create a new user document
+    const newUser = new User({ username, apikey, password });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.send("Registration successful");
+  } catch (error) {
+    console.error("Registration error:", error);
+    res.status(500).send("Registration failed");
+  }
+});
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 async function isValidApiKey(key) {
-    try {
-        const response = await axios.post("https://api.zalo.ai/v1/tts/synthesize", null, {
-            headers: { apikey: key, 'Content-Type': 'application/x-www-form-urlencoded' }
-        });
-        return response.status !== 401;
-    } catch (error) {
-        if (error.response && error.response.status === 401) {
-            return false;
-        }
-        throw error;
+  try {
+    const response = await axios.post(
+      "https://api.zalo.ai/v1/tts/synthesize",
+      null,
+      {
+        headers: {
+          apikey: key,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    return response.status !== 401;
+  } catch (error) {
+    if (error.response && error.response.status === 401) {
+      return false;
     }
+    throw error;
+  }
 }
