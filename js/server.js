1f3d5ebacb212281e46eb411fd329e3ab713ce97
Viet Hung <viethung01234@gmail.com>
2023-11-24 09:36:44 +0700

    ADD hash password, nodemon, gitignore

--- js/server.js
+++ js/server.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 const app = express();
 const port = process.env.PORT || 5500;
 
 // MongoDB connection setup (replace 'your_mongodb_uri' and 'yourdbname')
-mongoose.connect('mongodb+srv://slowey:tlvptlvp@projectx.3vv2dfv.mongodb.net/ProjectX', {
+mongoose.connect(
+  "mongodb+srv://slowey:tlvptlvp@projectx.3vv2dfv.mongodb.net/ProjectX",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+  }
+);
 
 // Create a schema and model for the Users table
 /**
@@ -20,40 +23,57 @@ mongoose.connect('mongodb+srv://slowey:tlvptlvp@projectx.3vv2dfv.mongodb.net/Pro
  * @property {string} password - The password of the user.
  */
 const UserSchema = new mongoose.Schema({
-    username: String,
-    apikey: String,
-    password: String,
+  username: String,
+  apikey: String,
+  password: String,
 });
 
-const User = mongoose.model('Users', UserSchema);
+UserSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
+});
+
+UserSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model("Users", UserSchema);
 
 // Middleware setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Serve static files (CSS, JS, etc.)
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Define a route for handling the registration form submission
-app.post('/sign-up', async(req, res) => {
-    try {
-        // Retrieve data from the form
-        const { username, apikey, password } = req.body;
+app.post("/sign-up", async (req, res) => {
+  try {
+    // Retrieve data from the form
+    const { username, apikey, password } = req.body;
 
-        // Create a new user document
-        const newUser = new User({ username, apikey, password });
+    // Create a new user document
+    const newUser = new User({ username, apikey, password });
 
-        // Save the user to the database
-        await newUser.save();
+    // Save the user to the database
+    await newUser.save();
 
-        res.send('Registration successful');
-    } catch (error) {
-        console.error('Registration error:', error);
-        res.status(500).send('Registration failed');
-    }
+    res.send("Registration successful");
+  } catch (error) {
+    console.error("Registration error:", error);
+    res.status(500).send("Registration failed");
+  }
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
