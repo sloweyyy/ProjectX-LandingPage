9e60ae56b72c1e064d7a9bbc942a2c65566b3b3c
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2023-11-24 18:12:25 +0700

    mongoose, sign-up form,...

--- js/server.js
+++ js/server.js
@@ -2,16 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
+const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 5500;
 
 // MongoDB connection setup (replace 'your_mongodb_uri' and 'yourdbname')
 mongoose.connect(
-  "mongodb+srv://slowey:tlvptlvp@projectx.3vv2dfv.mongodb.net/ProjectX",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
+    "mongodb+srv://slowey:tlvptlvp@projectx.3vv2dfv.mongodb.net/ProjectX", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
 );
 
 // Create a schema and model for the Users table
@@ -23,26 +23,26 @@ mongoose.connect(
  * @property {string} password - The password of the user.
  */
 const UserSchema = new mongoose.Schema({
-  username: String,
-  apikey: String,
-  password: String,
+    username: String,
+    apikey: String,
+    password: String,
 });
 
-UserSchema.pre("save", async function (next) {
-  // Only run this function if password was actually modified
-  if (!this.isModified("password")) return next();
+UserSchema.pre("save", async function(next) {
+    // Only run this function if password was actually modified
+    if (!this.isModified("password")) return next();
 
-  // Hash the password with cost of 12
-  this.password = await bcrypt.hash(this.password, 12);
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
 
-  next();
+    next();
 });
 
-UserSchema.methods.correctPassword = async function (
-  candidatePassword,
-  userPassword
+UserSchema.methods.correctPassword = async function(
+    candidatePassword,
+    userPassword
 ) {
-  return await bcrypt.compare(candidatePassword, userPassword);
+    return await bcrypt.compare(candidatePassword, userPassword);
 };
 
 const User = mongoose.model("Users", UserSchema);
@@ -55,25 +55,55 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // Define a route for handling the registration form submission
-app.post("/sign-up", async (req, res) => {
-  try {
-    // Retrieve data from the form
-    const { username, apikey, password } = req.body;
+app.post("/sign-up", async(req, res) => {
+    try {
+        const { username, apikey, password } = req.body;
 
-    // Create a new user document
-    const newUser = new User({ username, apikey, password });
+        // Check for missing fields
+        if (!username || !apikey || !password) {
+            return res.status(400).send("All fields are required");
+        }
 
-    // Save the user to the database
-    await newUser.save();
+        // Check if user already exists
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(409).send("User already exists");
+        }
 
-    res.send("Registration successful");
-  } catch (error) {
-    console.error("Registration error:", error);
-    res.status(500).send("Registration failed");
-  }
+        // Validate API key
+        if (!(await isValidApiKey(apikey))) {
+            return res.status(401).send("Invalid API key");
+        }
+
+        // Create a new user document
+        const newUser = new User({ username, apikey, password });
+
+        // Save the user to the database
+        await newUser.save();
+
+        res.send("Registration successful");
+    } catch (error) {
+        console.error("Registration error:", error);
+        res.status(500).send("Registration failed");
+    }
 });
+
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
+
+async function isValidApiKey(key) {
+    try {
+        const response = await axios.post("https://api.zalo.ai/v1/tts/synthesize", null, {
+            headers: { apikey: key, 'Content-Type': 'application/x-www-form-urlencoded' }
+        });
+        return response.status !== 401;
+    } catch (error) {
+        if (error.response && error.response.status === 401) {
+            return false;
+        }
+        throw error;
+    }
+}
