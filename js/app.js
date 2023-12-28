10b7b8bc881a0c5472b3892a0683ae38f573fd12
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2023-12-28 21:55:52 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -140,11 +140,31 @@ window.onload = function loader() {
     }, 350);
 };
 
-
 const signUpButton = document.getElementById("signup-submit-btn");
 signUpButton.addEventListener("click", handleSignUp);
 
 
+async function handleSignUp() {
+    const username = document.getElementById("username").value;
+    const useraccountname = document.getElementById("useraccountname").value;
+    const zaloapi = document.getElementById("zaloapi").value;
+    const fptapi = document.getElementById("fptapi").value;
+    const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+
+
+    const response = await fetch("https://your-api-endpoint.com/sign-up", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, useraccountname, zaloapi, fptapi, password, email }),
+    });
+
+    const responseData = await response.json();
+
+    console.log(responseData);
+}
 
 function hideTerms() {
     document.getElementById('termcondition').style.display = 'none';
